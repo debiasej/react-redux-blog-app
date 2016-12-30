6ee1b6cba0823105edfc4a4219258c1505fc8898
Mario de Biase <debiasej@gmail.com>
2016-12-30 22:16:58 +0100

    Call action fetchPosts in the post_index's componentWillMount

--- src/actions/index.js
+++ src/actions/index.js
@@ -5,7 +5,7 @@ export const FETCH_POST = 'FETCH_POST';
 const ROOT_URL = 'http://reduxblog.herokuapp.com/api';
 const API_KEY = '?key=zfEFB3fGyIdwBeNfj56';
 
-export function fetchPost() {
+export function fetchPosts() {
   const request = axios.get(`${ROOT_URL}/posts${API_KEY}`);
 
   return {
--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,5 +1,22 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { fetchPosts } from '../actions/index';
 
-export default () => {
-  return <div>List of blog posts.</div>;
-};
+class PostsIndex extends Component {
+  componentWillMount() {
+    this.props.fetchPosts();
+  }
+
+  render () {
+    return (
+      <div>List of blog posts</div>
+    );
+  }
+}
+
+function mapDispatchToProps(dispath) {
+  return bindActionCreators({ fetchPosts }, dispath);
+}
+
+export default connect(null, mapDispatchToProps)(PostsIndex);
