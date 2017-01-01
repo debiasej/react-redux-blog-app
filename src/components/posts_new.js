23b8bba8432d9dd05f3d822b9b65f2a3c0a0f698
Mario de Biase <debiasej@gmail.com>
2017-01-01 15:08:51 +0100

    Use redux-form to markup the posts_new form

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,11 +1,46 @@
 import React, { Component } from 'react';
+import { reduxForm } from 'redux-form';
 
 class PostsNew extends Component {
   render() {
       return (
-        <div>Create Form</div>
+        <form>
+          <h3>Create a New Post</h3>
+          <div className="form-group">
+            <label>Title</label>
+            <input type="text" className="form-control" />
+          </div>
+
+          <div className="form-group">
+            <label>Categories</label>
+            <input type="text" className="form-control" />
+          </div>
+
+          <div className="form-group">
+            <label>Content</label>
+            <textarea type="text" className="form-control" />
+          </div>
+
+          <button type="submit" className="btn btn-primary">Submit</button>
+        </form>
       );
   }
 }
 
-export default PostsNew;
+// reduxForm is like connect function in react-redux library
+export default reduxForm({
+  form: 'PostsNewForm',
+  fields: ['title', 'categories', 'content']
+})(PostsNew);
+
+/* When user types something in.... redux-form records it on app state
+state === {
+  form: {
+    PostsNewForm: {
+      title: '...',
+      categories: '...',
+      content: '...'
+    }
+  }
+}
+*/
