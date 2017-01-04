7131ce1bd79c84821a8f2cf89a7e33b83a453f14
Mario de Biase <debiasej@gmail.com>
2017-01-04 21:21:35 +0100

    Markup of every post in the index view

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -9,6 +9,17 @@ class PostsIndex extends Component {
     this.props.fetchPosts();
   }
 
+  renderPosts() {
+    return this.props.posts.map((post) => {
+      return (
+        <li className="list-group-item" key={post.id}>
+          <span className="float-right">{post.categories}</span>
+          <strong>{post.title}</strong>
+        </li>
+      );
+    });
+  }
+
   render () {
     return (
       <div>
@@ -17,10 +28,17 @@ class PostsIndex extends Component {
             Add a Post
           </Link>
         </div>
-        List of blog posts
+        <h3>Posts</h3>
+        <ul className="list-group">
+          {this.renderPosts()}
+        </ul>
       </div>
     );
   }
+}
+
+function mapStateToProps(state) {
+  return { posts: state.posts.all }
 }
 
 // Original version
@@ -34,4 +52,4 @@ class PostsIndex extends Component {
 //export default connect(null, { fetchPosts: fetchPosts })(PostsIndex);
 
 // Second refactor: using ES6 sintax
-export default connect(null, { fetchPosts })(PostsIndex);
+export default connect(mapStateToProps, { fetchPosts })(PostsIndex);
