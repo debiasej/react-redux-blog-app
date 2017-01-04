b03594aabf70db9f2e699d76702f322f808fe86a
Mario de Biase <debiasej@gmail.com>
2017-01-04 21:04:26 +0100

    Navigate to the index when a blog is created

--- src/actions/index.js
+++ src/actions/index.js
@@ -16,7 +16,7 @@ export function fetchPosts() {
 };
 
 export function createPost(props) {
-  const resquest = axios.post(`${ROOT_URL}/posts${API_KEY}`, props);
+  const request = axios.post(`${ROOT_URL}/posts${API_KEY}`, props);
 
   return {
     type: CREATE_POST,
