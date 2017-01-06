194b1749811bdde8b1334cbb61c10363a5cbd228
Mario de Biase <debiasej@gmail.com>
2017-01-06 20:59:25 +0100

    Link every post of the index to its content

---
+++ src/components/posts_show.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+class PostsShow extends Component {
+  render() {
+    return <div>Show post {this.props.params.id}</div>
+  }
+
+}
+
+export default PostsShow;
