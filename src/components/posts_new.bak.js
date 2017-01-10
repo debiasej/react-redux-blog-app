0f1c845d106fe242b7bacfaab3d3d93ffe7932e3
Mario de Biase <debiasej@gmail.com>
2017-01-10 20:17:07 +0100

    Backup posts_new: Prepate the refactor

---
+++ src/components/posts_new.bak.js
@@ -0,0 +1,96 @@
+import React, { Component, PropTypes } from 'react';
+import { reduxForm } from 'redux-form';
+import { createPost } from '../actions/index';
+import { Link } from 'react-router';
+
+class PostsNew extends Component {
+  static contextTypes = {
+    router: PropTypes.object
+  }
+
+  onSubmit(props) {
+    this.props.createPost(props)
+      .then(() => {
+        // blog post has been created, navigate the user to the index
+        // We navigate by calling this.context.router.push the new path
+        // to navigate to.
+        this.context.router.push('/');
+      });
+  }
+
+  render() {
+    const { fields: { title, categories, content }, handleSubmit } = this.props;
+    // const handleSubmit = this.props.handleSubmit;
+    // const title = this.props.fields.title; ...
+
+    return (
+      <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
+        <h3>Create a New Post</h3>
+        <div className={`form-group ${title.touched && title.invalid ? 'has-danger' : ''}`}>
+          <label>Title</label>
+          <input type="text" className="form-control" {...title} />
+          <div className="form-control-feedback">
+            {title.touched ? title.error : ''}
+          </div>
+        </div>
+
+        <div className={`form-group ${categories.touched && categories.invalid ? 'has-danger' : ''}`}>
+          <label>Categories</label>
+          <input type="text" className="form-control" {...categories} />
+          <div className="form-control-feedback">
+            {categories.touched ? categories.error : ''}
+          </div>
+        </div>
+
+        <div className={`form-group ${content.touched && content.invalid ? 'has-danger' : ''}`}>
+          <label>Content</label>
+          <textarea type="text" className="form-control" {...content} />
+          <div className="form-control-feedback">
+            {content.touched ? content.error : ''}
+          </div>
+        </div>
+
+        <button type="submit" className="btn btn-primary">Submit</button>
+        <Link to="/" className="btn btn-danger">Cancel</Link>
+      </form>
+    );
+  }
+}
+
+function validate(values) {
+  const errors = {};
+
+  if (!values.title) {
+    errors.title = 'Enter a username';
+  }
+  if (!values.categories) {
+    errors.categories = 'Enter categories';
+  }
+  if (!values.content) {
+    errors.content = 'Enter some content';
+  }
+
+  return errors;
+}
+
+// connect: 1st argument is mapStateToProps, 2nd is mapDispatchToProps
+// reduxForm: 1st is form config, 2nd is mapStateToProps, 3rd is mapDispatchToProps
+
+// reduxForm is like connect function in react-redux library
+export default reduxForm({
+  form: 'PostsNewForm',
+  fields: ['title', 'categories', 'content'],
+  validate
+}, null, { createPost })(PostsNew);
+
+/* When user types something in.... redux-form records it on app state
+state === {
+  form: {
+    PostsNewForm: {
+      title: '...',
+      categories: '...',
+      content: '...'
+    }
+  }
+}
+*/
